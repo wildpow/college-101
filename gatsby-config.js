6a6df4f3267ac616ecb5ae43ca4a81d81449f80c
wildpow <element1028@gmail.com>
2018-11-04 08:02:15 -0800

    bringing in GraphCMS data with gatsbyConfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,28 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Default Starter"
+    title: "Gatsby Default Starter",
   },
   plugins: [
     `gatsby-plugin-styled-components`,
     "gatsby-plugin-react-helmet",
     {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // The top level query type, can be anything you want!
+        typeName: "GCMS",
+        // The field you'll query against, can also be anything you want.
+        fieldName: "gcms",
+        // Your API endpoint, available from the dashboard and settings window.
+        url:
+          "https://api-uswest.graphcms.com/v1/cjo0z8t7r3ba301gld72qlzv2/master",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
@@ -23,9 +35,9 @@ module.exports = {
         background_color: "#663399",
         theme_color: "#663399",
         display: "minimal-ui",
-        icon: "src/images/gatsby-icon.png" // This path is relative to the root of the site.
-      }
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+      },
     },
-    "gatsby-plugin-offline"
-  ]
+    "gatsby-plugin-offline",
+  ],
 };
