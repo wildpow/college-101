ee36237699e58427b0e591e0fa5dc329eb4e5b06
wildpow <element1028@gmail.com>
2018-11-09 14:40:24 -0800

    build faliing with apollo client

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,7 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
 import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { client } from "./src/apollo/client";
 
-const { GATSBY_GRAPHCMS } = process.env;
-
-const client = new ApolloClient({
-  uri: GATSBY_GRAPHCMS,
-});
-console.log(process.env);
-export const wrapRootElement = ({ element }) => {
-  return <ApolloProvider client={client}>{element}</ApolloProvider>;
-};
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+);
