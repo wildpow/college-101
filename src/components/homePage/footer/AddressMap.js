5f0339a6c2651e6236d2abf6e7a4c75a119829fa
wildpow <element1028@gmail.com>
2018-11-15 18:32:06 -0800

    starting on footer

---
+++ src/components/homePage/footer/AddressMap.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  padding-top: 20px;
+  justify-content: center;
+  justify-content: space-evenly;
+`;
+const Map = styled.div`
+  width: 250px;
+  height: 250px;
+  color: black;
+  margin-right: 20px;
+  background-color: white;
+`;
+const AddressMap = () => (
+  <Wrapper>
+    <Map>MAP</Map>
+    <div>
+      <h3>Address</h3>
+      College 101
+      <br />
+      290 Main Street East Setauket NY 11733
+      <br />
+      Phone: (631) 364-9080
+      <br />
+      E-mail: College101ResourceCenter@Gmail.com
+    </div>
+  </Wrapper>
+);
+
+export default AddressMap;
