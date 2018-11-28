879a797b906d00b8b44564c21231b6c107288eb9
AnthonyCatalfo <anthonycatalfo@gmail.com>
2018-11-28 18:55:42 -0500

    Updating

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -70,7 +70,7 @@ class Checkout extends React.Component {
       billingAddress: true,
       token: token => {
         fetch(
-          "https://bigtony--college101prep.netlify.com/.netlify/functions/purchase2",
+          "https://bigtony--college101prep.netlify.com/.netlify/functions/purchase",
           {
             method: "POST",
             //mode: "no-cors",
