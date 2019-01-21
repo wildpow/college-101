cc61c518eb13472e3c229778495fa7aa225f1bbb
wildpow <element1028@gmail.com>
2019-01-21 08:21:05 -0800

    started adding back mutation code. still needs startTime and endTime

--- src/admin/components/test/createSession2.js
+++ src/admin/components/test/createSession2.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { hpe } from "grommet-theme-hpe";
-// import { aruba } from "grommet-theme-aruba";
-
+import { Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import {
   Box,
   Button,
@@ -19,6 +19,30 @@ import SelectCourse from "./selectCourse";
 import SelectTeacher from "./selectTeacher";
 import StartDate from "./startDate";
 import EndDate from "./endDate";
+
+const ADD_SESSION = gql`
+  mutation(
+    $startTime: DateTime!
+    $endTime: DateTime!
+    $maxSizeOfClass: Int!
+    $courseId: ID
+    $teacherId: ID
+  ) {
+    createSession(
+      data: {
+        startTime: $startTime
+        endTime: $endTime
+        maxSizeOfClass: $maxSizeOfClass
+        status: PUBLISHED
+        teacher: { connect: { id: $teacherId } }
+        course: { connect: { id: $courseId } }
+      }
+    ) {
+      startTime
+      endTime
+    }
+  }
+`;
 
 const StartTimeContainer = styled(Box)``;
 const EndTimeContainer = styled(Box)``;
@@ -47,16 +71,17 @@ class CreateSession extends React.Component {
     startDate: undefined,
     endDateOpen: false,
     endDate: undefined,
-    maxSize: 1,
+    maxSizeOfClass: 1,
   };
 
   componentDidMount() {
+    const { data } = this.props;
     const teachersNames = [];
     const courseNames = [];
-    this.props.data.teachers.map(teacher =>
+    data.teachers.map(teacher =>
       teachersNames.push(`${teacher.firstName} ${teacher.lastName}`),
     );
-    this.props.data.courses.map(course => courseNames.push(course.name));
+    data.courses.map(course => courseNames.push(course.name));
 
     this.setState({
       teacherOptions: teachersNames,
@@ -130,7 +155,7 @@ class CreateSession extends React.Component {
       startDate,
       endDateOpen,
       endDate,
-      maxSize,
+      maxSizeOfClass,
     } = this.state;
     const { data } = this.props;
     const teachersNamesCopy = [];
@@ -162,140 +187,167 @@ class CreateSession extends React.Component {
               onClickOutside={this.onClose}
               onEsc={this.onClose}
             >
-              <Box
-                // gap="small"
-                as="form"
-                fill="vertical"
-                overflow="auto"
-                width="medium"
-                pad="medium"
-                onSubmit={event => {
-                  event.preventDefault();
-                  if (
-                    selectedCourse.length === 0 &&
-                    selectedTeacher.length === 0
-                  ) {
-                    this.setState({
-                      teacherError: true,
-                      courseError: true,
-                    });
-                    return null;
-                  }
-                  if (selectedTeacher.length === 0) {
-                    console.log(selectedTeacher.length);
-                    this.setState({
-                      teacherError: true,
-                    });
-                    return null;
-                  }
-                  if (selectedCourse.length === 0) {
-                    this.setState({
-                      courseError: true,
-                    });
-                    return null;
-                  }
-                  return console.log("submit");
-                }}
-              >
-                <Box flex={false} direction="row" justify="between">
-                  <Heading level={2} margin="none">
-                    Add Session
-                  </Heading>
-                  <Button icon={<Close />} onClick={this.onClose} />
-                </Box>
-                <Box
-                  flex="grow"
-                  overflow="auto"
-                  pad={{ vertical: "small" }}
-                  gap="small"
-                >
-                  <SelectCourse
-                    selectedCourse={selectedCourse}
-                    courseSelectChange={this.courseSelectChange}
-                    onSearchCourses={this.onSearchCourses}
-                    courseOptions={courseOptions}
-                    courseError={courseError}
-                  />
-                  <SelectTeacher
-                    selectedTeacher={selectedTeacher}
-                    teacherSelectChange={this.teacherSelectChange}
-                    onSearchTeachers={this.onSearchTeachers}
-                    teacherOptions={teacherOptions}
-                    teacherError={teacherError}
-                  />
-                  <TimeWrapper
-                    direction="row"
-                    // justify="between"
-                    // margin={{ top: "medium" }}
-                    flex={false}
+              <Mutation mutation={ADD_SESSION}>
+                {createSession => (
+                  <Box
+                    // gap="small"
+                    as="form"
+                    fill="vertical"
+                    overflow="auto"
+                    width="medium"
+                    pad="medium"
+                    onSubmit={event => {
+                      event.preventDefault();
+                      if (
+                        selectedCourse.length === 0 &&
+                        selectedTeacher.length === 0
+                      ) {
+                        this.setState({
+                          teacherError: true,
+                          courseError: true,
+                        });
+                        return null;
+                      }
+                      if (selectedTeacher.length === 0) {
+                        this.setState({
+                          teacherError: true,
+                        });
+                        return null;
+                      }
+                      if (selectedCourse.length === 0) {
+                        this.setState({
+                          courseError: true,
+                        });
+                        return null;
+                      }
+                      const teacherId =
+                        teacherIDs[teachersNamesCopy.indexOf(selectedTeacher)];
+                      const courseId =
+                        courseIDs[courseNamesCopy.indexOf(selectedCourse)];
+                      // console.log(maxSizeOfClass);
+                      // createSession({
+                      //   variables: {
+                      //     startTime,
+                      //     endTime,
+                      //     teacherId,
+                      //     courseId,
+                      //     maxSizeOfClass,
+                      //   },
+                      // });
+                    }}
                   >
-                    <StartTimeContainer align="start" direction="column">
-                      <Heading alignSelf="center" margin="xsmall" level={4}>
-                        Start Time
+                    <Box flex={false} direction="row" justify="between">
+                      <Heading level={2} margin="none">
+                        Add Session
                       </Heading>
-                      <Box margin="small">
-                        <StartDate
-                          startDateOpen={startDateOpen}
-                          startOnOpen={this.startOnOpen}
-                          startOnClose={this.startOnClose}
-                          startDate={startDate}
-                          startTimeSelect={this.startTimeSelect}
-                        />
-                        <TimePicker />
-                      </Box>
-                    </StartTimeContainer>
-                    <EndTimeContainer align="start" direction="column">
-                      <Heading alignSelf="center" margin="xsmall" level={4}>
-                        End Time
-                      </Heading>
-                      <Box margin="small">
-                        <EndDate
-                          endDateOpen={endDateOpen}
-                          endOnOpen={this.endOnOpen}
-                          endOnClose={this.endOnClose}
-                          endDate={endDate}
-                          endTimeSelect={this.endTimeSelect}
-                        />
-                        <TimePicker />
-                      </Box>
-                    </EndTimeContainer>
-                  </TimeWrapper>
-                  <BLA margin="xsmall">
-                    <FormField label="Max Size of Class" name="max">
-                      <RangeInput
-                        onChange={event =>
-                          this.setState({ maxSize: event.target.value })
-                        }
-                        min={1}
-                        max={25}
-                        value={maxSize}
+                      <Button icon={<Close />} onClick={this.onClose} />
+                    </Box>
+                    <Box
+                      flex="grow"
+                      overflow="auto"
+                      pad={{ vertical: "small" }}
+                      gap="small"
+                    >
+                      <SelectCourse
+                        selectedCourse={selectedCourse}
+                        courseSelectChange={this.courseSelectChange}
+                        onSearchCourses={this.onSearchCourses}
+                        courseOptions={courseOptions}
+                        courseError={courseError}
                       />
-                      <Box direction="row" align="center" alignSelf="center">
-                        <Heading level={1}>{maxSize}</Heading>
-                      </Box>
-                    </FormField>
-                  </BLA>
-                </Box>
-                <Box
-                  direction="row"
-                  justify="between"
-                  // margin={{ top: "xsmall" }}
-                >
-                  <Button
-                    icon={<FormSubtract />}
-                    label="Clear"
-                    onClick={() =>
-                      this.setState({
-                        selectedCourse: "",
-                        selectedTeacher: "",
-                        endDate: undefined,
-                        startDate: undefined,
-                      })
-                    }
-                  />
-                  <Button type="submit" label="Add" primary icon={<Add />} />
-                </Box>
-              </Box>
+                      <SelectTeacher
+                        selectedTeacher={selectedTeacher}
+                        teacherSelectChange={this.teacherSelectChange}
+                        onSearchTeachers={this.onSearchTeachers}
+                        teacherOptions={teacherOptions}
+                        teacherError={teacherError}
+                      />
+                      <TimeWrapper
+                        direction="row"
+                        // justify="between"
+                        // margin={{ top: "medium" }}
+                        flex={false}
+                      >
+                        <StartTimeContainer align="start" direction="column">
+                          <Heading alignSelf="center" margin="xsmall" level={4}>
+                            Start Time
+                          </Heading>
+                          <Box margin="small">
+                            <StartDate
+                              startDateOpen={startDateOpen}
+                              startOnOpen={this.startOnOpen}
+                              startOnClose={this.startOnClose}
+                              startDate={startDate}
+                              startTimeSelect={this.startTimeSelect}
+                            />
+                            <TimePicker />
+                          </Box>
+                        </StartTimeContainer>
+                        <EndTimeContainer align="start" direction="column">
+                          <Heading alignSelf="center" margin="xsmall" level={4}>
+                            End Time
+                          </Heading>
+                          <Box margin="small">
+                            <EndDate
+                              endDateOpen={endDateOpen}
+                              endOnOpen={this.endOnOpen}
+                              endOnClose={this.endOnClose}
+                              endDate={endDate}
+                              endTimeSelect={this.endTimeSelect}
+                            />
+                            <TimePicker />
+                          </Box>
+                        </EndTimeContainer>
+                      </TimeWrapper>
+                      <BLA margin="xsmall">
+                        <FormField label="Max Size of Class" name="max">
+                          <RangeInput
+                            onChange={event =>
+                              this.setState({
+                                maxSizeOfClass: event.target.value,
+                              })
+                            }
+                            min={1}
+                            max={25}
+                            value={maxSizeOfClass}
+                          />
+                          <Box
+                            direction="row"
+                            align="center"
+                            alignSelf="center"
+                          >
+                            <Heading level={1}>{maxSizeOfClass}</Heading>
+                          </Box>
+                        </FormField>
+                      </BLA>
+                    </Box>
+                    <Box
+                      direction="row"
+                      justify="between"
+                      // margin={{ top: "xsmall" }}
+                    >
+                      <Button
+                        icon={<FormSubtract />}
+                        label="Clear"
+                        onClick={() =>
+                          this.setState({
+                            selectedCourse: "",
+                            selectedTeacher: "",
+                            endDate: undefined,
+                            startDate: undefined,
+                          })
+                        }
+                      />
+                      <Button
+                        type="submit"
+                        label="Add"
+                        primary
+                        icon={<Add />}
+                      />
+                    </Box>
+                  </Box>
+                )}
+              </Mutation>
             </Layer>
           )}
         </Box>
