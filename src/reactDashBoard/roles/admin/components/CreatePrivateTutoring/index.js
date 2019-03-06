941b954665d3243a79752d9a88e0b11ed4cb92d5
wildpow <element1028@gmail.com>
2019-03-06 10:40:55 -0800

    private session
    - [x] Moved shared components to a shareComponent folder.
    - [x] Aded error state and Mutation to private tutoring button.
    - [x] Clear button now clears every field.
    - [x] close button clears every field now.

--- src/reactDashBoard/roles/admin/components/CreatePrivateTutoring/index.js
+++ src/reactDashBoard/roles/admin/components/CreatePrivateTutoring/index.js
@@ -5,10 +5,11 @@ import { Button, Layer, Box } from "grommet";
 import { Add, FormSubtract } from "grommet-icons";
 import LayerHeader from "../../layerHeader";
 import TypeOfClass from "./typeOfClass";
-import SelectCourse from "../createSession/selectCourse";
-import SelectTeacher from "../createSession/selectTeacher";
-import StartDate from "../createSession/startDate";
-import StartTimePicker from "../createSession/startTimePicker";
+import SelectCourse from "../sharedComponents/selectCourse";
+import SelectTeacher from "../sharedComponents/selectTeacher";
+import StartDate from "../sharedComponents/startDate";
+import StartTimePicker from "../sharedComponents/startTimePicker";
+import { ALL_SESSIONS } from "../../../../queryComponents/QuerySessions";
 
 const ADD_PRIVATE_TUT = gql`
   mutation(
@@ -74,6 +75,8 @@ class PrivateTutoring extends React.Component {
         selectedTeacher: "",
         startDateOpen: false,
         startDate: new Date().toISOString(),
+        startTimeError: false,
+        startTime: "",
       });
     }
   };
@@ -130,6 +133,16 @@ class PrivateTutoring extends React.Component {
     });
   };
 
+  errorCheck = (value, errorState) => {
+    if (value.length === 0) {
+      this.setState({
+        [`${errorState}`]: true,
+      });
+      return null;
+    }
+    return null;
+  };
+
   render() {
     const {
       typeSelect,
@@ -169,76 +182,107 @@ class PrivateTutoring extends React.Component {
               headingText="Private Tutoring"
               modelFunc={this.layerToggle}
             />
-            <Box
-              gap="small"
-              fill="vertical"
-              overflow="auto"
-              width="medium"
-              pad="medium"
-              as="form"
-              onSubmit={event => {
-                event.preventDefault();
+            <Mutation
+              mutation={ADD_PRIVATE_TUT}
+              refetchQueries={() => {
+                return [
+                  {
+                    query: ALL_SESSIONS,
+                  },
+                ];
               }}
             >
-              <Box fill overflow="scroll" justify="between">
-                <Box>
-                  <TypeOfClass
-                    typeSelect={typeSelect}
-                    privateTutorings={privateTutorings}
-                    setSessionType={this.setSessionType}
-                    sessionTypeError={sessionTypeError}
-                  />
-                  <SelectCourse
-                    selectedCourse={selectedCourse}
-                    courseError={courseError}
-                    courseOptions={courseOptions}
-                    courseSelectChange={this.courseSelectChange}
-                    onSearchCourses={this.onSearchCourses}
-                  />
-                  <SelectTeacher
-                    selectedTeacher={selectedTeacher}
-                    onSearchTeachers={this.onSearchTeachers}
-                    teacherOptions={teacherOptions}
-                    teacherError={teacherError}
-                    teacherSelectChange={this.teacherSelectChange}
-                  />
-                  <StartDate
-                    startDateToggle={this.startDateToggle}
-                    startDate={startDate}
-                    startDateSelect={this.startDateSelect}
-                    startDateOpen={startDateOpen}
-                  />
-                  <StartTimePicker
-                    startTimeError={startTimeError}
-                    startTime={startTime}
-                    startTimeMessage={startTimeMessage}
-                    onChangeStartTime={this.onChangeStartTime}
-                  />
-                </Box>
+              {createSession => (
                 <Box
-                  direction="row"
-                  justify="between"
-                  pad={{ horizontal: "xsmall", vertical: "xsmall" }}
-                >
-                  <Button type="submit" label="ADD" primary icon={<Add />} />
-                  <Button
-                    icon={<FormSubtract />}
-                    label="CLEAR"
-                    onClick={() =>
-                      this.setState({
-                        selectedCourse: "",
-                        selectedTeacher: "",
-                        startDate: new Date().toISOString(),
-                        startTime: "",
-                        startTimeError: false,
-                        courseError: false,
-                        teacherError: false,
-                      })
+                  gap="small"
+                  fill="vertical"
+                  overflow="auto"
+                  width="medium"
+                  pad="medium"
+                  as="form"
+                  onSubmit={event => {
+                    event.preventDefault();
+                    if (
+                      typeSelect.length === 0 ||
+                      selectedCourse.length === 0 ||
+                      selectedTeacher.length === 0 ||
+                      startTime.length === 0
+                    ) {
+                      this.errorCheck(selectedCourse, "courseError");
+                      this.errorCheck(selectedTeacher, "teacherError");
+                      this.errorCheck(typeSelect, "sessionTypeError");
+                      this.errorCheck(startTime, "startTimeError");
                     }
-                  />
+                  }}
+                >
+                  <Box fill overflow="scroll" justify="between">
+                    <Box>
+                      <TypeOfClass
+                        typeSelect={typeSelect}
+                        privateTutorings={privateTutorings}
+                        setSessionType={this.setSessionType}
+                        sessionTypeError={sessionTypeError}
+                      />
+                      <SelectCourse
+                        selectedCourse={selectedCourse}
+                        courseError={courseError}
+                        courseOptions={courseOptions}
+                        courseSelectChange={this.courseSelectChange}
+                        onSearchCourses={this.onSearchCourses}
+                      />
+                      <SelectTeacher
+                        selectedTeacher={selectedTeacher}
+                        onSearchTeachers={this.onSearchTeachers}
+                        teacherOptions={teacherOptions}
+                        teacherError={teacherError}
+                        teacherSelectChange={this.teacherSelectChange}
+                      />
+                      <StartDate
+                        startDateToggle={this.startDateToggle}
+                        startDate={startDate}
+                        startDateSelect={this.startDateSelect}
+                        startDateOpen={startDateOpen}
+                      />
+                      <StartTimePicker
+                        startTimeError={startTimeError}
+                        startTime={startTime}
+                        startTimeMessage={startTimeMessage}
+                        onChangeStartTime={this.onChangeStartTime}
+                      />
+                    </Box>
+                    <Box
+                      direction="row"
+                      justify="between"
+                      pad={{ horizontal: "xsmall", vertical: "xsmall" }}
+                    >
+                      <Button
+                        type="submit"
+                        label="ADD"
+                        primary
+                        icon={<Add />}
+                      />
+                      <Button
+                        icon={<FormSubtract />}
+                        label="CLEAR"
+                        onClick={() =>
+                          this.setState({
+                            selectedCourse: "",
+                            selectedTeacher: "",
+                            startDate: new Date().toISOString(),
+                            startTime: "",
+                            startTimeError: false,
+                            courseError: false,
+                            teacherError: false,
+                            sessionTypeError: false,
+                            typeSelect: "",
+                          })
+                        }
+                      />
+                    </Box>
+                  </Box>
                 </Box>
-              </Box>
-            </Box>
+              )}
+            </Mutation>
           </Layer>
         )}
       </Box>
